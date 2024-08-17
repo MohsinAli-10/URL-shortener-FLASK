9ed545836e6a8913e4b16dffe22b18ac0b0569e0
Mohsin Ali <mohsin.ali.tele@gmail.com>
2024-08-17 22:51:44 +0500

    blueprint functionality 2

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+# If __init__ is in urlshort folder
+# from urlshort.__init__ import create_app
+
 from urlshort.__init__ import create_app
 from flask import Flask
 
--- urlshort/__init__.py
+++ urlshort/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
     app.secret_key = 'jkdshfjkasdfhsadkmad;lsam' #allows information to be securely passed to the html
-    from . import urlshort    
+    from urlshort import urlshort
     app.register_blueprint(urlshort.bp)
 
     return app
